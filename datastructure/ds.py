2022225e3b5a57d4840acd9dbeae5ce0b07306f4
Priyanshi Shah <priyanshishah495@gmail.com>
2026-07-06 00:29:37 +0530

    comment

--- datastructure/ds.py
+++ datastructure/ds.py
@@ -54,10 +54,10 @@
 
 # q6 sets
 
-my_set = {1,2,3,3,4}
-print(my_set)
-my_set.add(5)
-print(my_set)
-my_set.remove(2)
-print(my_set)
-# is 2 in my_set
+# my_set = {1,2,3,3,4}
+# print(my_set)
+# my_set.add(5)
+# print(my_set)
+# my_set.remove(2)
+# print(my_set)
+# # is 2 in my_set
