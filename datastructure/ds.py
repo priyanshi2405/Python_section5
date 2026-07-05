e9f0f19e4e7c2cb96537bd0fe91113965de6e02c
Priyanshi Shah <priyanshishah495@gmail.com>
2026-07-05 23:46:16 +0530

    question2

--- datastructure/ds.py
+++ datastructure/ds.py
@@ -1,5 +1,16 @@
-fruits = ["apple", "banana" , "cherry"]
-print(fruits[0])
-fruits[1] = "orange"
-print(fruits)
-print(len(fruits))
+# question 1 
+# fruits = ["apple", "banana" , "cherry"]
+# print(fruits[0])
+# fruits[1] = "orange"
+# print(fruits)
+# print(len(fruits))
+
+# create a list of numbers 1 t0 10 and perform slicing
+
+li = []
+for i in range(1,11):
+    li.append(i)
+
+print(li)
+print(li[ : 3])
+print(li[-3:])
