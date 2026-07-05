a38c214d033beb0a2c023a30e180428a0e3d18f3
Priyanshi Shah <priyanshishah495@gmail.com>
2026-07-06 00:18:58 +0530

    some que left

--- datastructure/ds.py
+++ datastructure/ds.py
@@ -7,10 +7,57 @@
 
 # create a list of numbers 1 t0 10 and perform slicing
 
-li = []
-for i in range(1,11):
-    li.append(i)
+# li = []
+# for i in range(1,11):
+#     li.append(i)
 
-print(li)
-print(li[ : 3])
-print(li[-3:])
+# print(li)
+# print(li[ : 3])
+# print(li[-3:])
+
+# list methods
+
+# numbers = [5,2,9,1,7]
+# numbers.sort()
+# print(numbers)
+# numbers.append(10)
+# print(numbers)
+# numbers.remove(2)
+# print(numbers)
+
+
+# q3 create a list and use insert method to insert a name
+
+# li = ["alice", "bob" , "charli"]
+# li.insert(1, "david")
+# print(li)
+
+
+# q4 tuples
+
+# cordinates = (10 , 20)
+# print(cordinates)
+# print(cordinates[0],cordinates[1]) 
+# cordinates[0] = 50
+# print(cordinates)
+
+
+# q5 convert tuple into list and back list to tuple
+
+# cordinates = (10 , 20)
+# corlist = list(cordinates)
+# corlist[0] = 50
+# print(corlist)
+# cordinates = tuple(corlist)
+# print(cordinates)
+
+
+# q6 sets
+
+my_set = {1,2,3,3,4}
+print(my_set)
+my_set.add(5)
+print(my_set)
+my_set.remove(2)
+print(my_set)
+# is 2 in my_set
